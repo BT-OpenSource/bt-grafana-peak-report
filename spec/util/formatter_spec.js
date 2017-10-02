c56f16a6717ea4de914ac21a2eb0ded1963d5d94
Ben Thorner <ben.thorner@bt.com>
2017-10-02 14:38:16 +0100

    Complete formatter spec

--- spec/util/formatter_spec.js
+++ spec/util/formatter_spec.js
@@ -2,16 +2,17 @@ import {Formatter} from '../../src/util/formatter'
 
 describe('Formatter', function () {
   beforeEach(function () {
-    var panel = { columns: [{ format: 'custom' }] }
+    var panel = { decimals: 2, columns: [{ format: 'custom' }] }
     this.format = jasmine.createSpy().and.returnValue('custom')
 
-    this.kbn = { valueFormats: { custom: this.format } }
-    this.subject = new Formatter(panel, this.kbn)
+    var kbn = { valueFormats: { custom: this.format } }
+    this.subject = new Formatter(panel, kbn)
   })
 
   describe('call', function () {
     it('formats the cell for the column', function () {
       expect(this.subject.call(1, 0)).toEqual('custom')
+      expect(this.format).toHaveBeenCalledWith(1, 2, null)
     })
   })
 })
